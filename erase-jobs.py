78e68ded0b17b213361cb7fc8c5d55070e9c877b
Victor <ViRb3@users.noreply.github.com>
2019-01-17 22:11:42 +0000

    Update with proper delete for GitLab 11.6+

--- erase-jobs.py
+++ erase-jobs.py
@@ -6,39 +6,28 @@ serverUrl = "https://gitlab.com/api/v4"
 projectId = input("Project: (e.g. mynamespace/myproject)\n> ")
 token = "your_api_token_here"
 
-erased_pipelines = 0
-erased_jobs = 0
-
 
 def make_req(url, data=None, method="GET"):
     req = request.Request(url, data, method=method)
     req.add_header('PRIVATE-TOKEN', token)
-    return json.loads(request.urlopen(req).read().decode())
+    resp = request.urlopen(req).read().decode()
+    return json.loads(resp or '{}')
 
 
-def erase_jobs(pl):
-    global erased_jobs
-    jobs = make_req(
-        f"{serverUrl}/projects/{parse.quote_plus(projectId)}/pipelines/{pl['id']}/jobs")
-    for job in jobs:
-        try:
-            make_req(
-                f"{serverUrl}/projects/{parse.quote_plus(projectId)}/jobs/{job['id']}/erase", None, "POST")
-        except:
-            pass
-        erased_jobs += 1
-
+numIgnore = int(input("Leave this many pipelines (0):\n> ") or "0")
+erased_pipelines = 0
 
 print()
 print("Working...")
 
-pipelines = make_req(
-    f"{serverUrl}/projects/{parse.quote_plus(projectId)}/pipelines")
-
-if len(pipelines) > 1:
-    for pl in pipelines[1:]:
-        erase_jobs(pl)
+while True:
+    pipelines = make_req(
+        f"{serverUrl}/projects/{parse.quote_plus(projectId)}/pipelines")
+    if len(pipelines) <= numIgnore:
+        break
+    for pl in pipelines[numIgnore:]:
+        make_req(
+            f"{serverUrl}/projects/{parse.quote_plus(projectId)}/pipelines/{pl['id']}", method="DELETE")
         erased_pipelines += 1
 
-print("Done!")
-print(f"Erased {erased_pipelines} pipelines and {erased_jobs} jobs.")
+print(f"Done! Erased {erased_pipelines} pipelines.")
